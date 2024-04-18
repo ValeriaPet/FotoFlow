4dc65b3facf9681be610bba760d98a89110d44d3
ValeriiaPetrosian <petrosyan.valeria@gmail.com>
2024-04-19 00:20:12 +0100

    ImageListServise

--- Servises/ImagesListService.swift
+++ Servises/ImagesListService.swift
@@ -8,14 +8,75 @@
 import Foundation
 
 final class ImagesListService {
-    static let didChangeNotification = Notification.Name("ImagesListServiceDidChange")
+    
+    private enum photosNextPageErrors: Error {
+        case requestError
+    }
+    
+    static let imageListUpdated = Notification.Name("ImagesListServiceDidChange")
     
     private (set) var photos: [Photo] = []
-    
     private var lastLoadedPage: Int?
+    private var task: URLSessionTask?
     
-    func fetchPhotosNextPage() {
+    func fetchPhotosNextPage(_ username: String, _ page: Int) {
+        
+        guard task == nil else {
+            print("aaaa")
+            return
+        }
         
         let nextPage = (lastLoadedPage ?? 0) + 1
+        
+//        let requestUrl = "https://api.unsplash.com/photos?page=\(nextPage)&per_page=10"
+        
+        guard let request = makeFetchListPhotoRequest(username: username, page: page) else
+        {return}
+        
+        let session = URLSession.shared
+        task = session.dataTask(with: request) { [weak self] data, response, error in
+                defer { self?.task = nil }
+                guard let self = self else { return }
+
+                if let error = error {
+                    print("Ошибка загрузки фото: \(error.localizedDescription)")
+                    return
+                }
+
+                guard let data = data, let decodedResponse = try? JSONDecoder().decode(PhotoPageResponse.self, from: data) else {
+                    print("Не удалось декодировать ответ")
+                    return
+                }
+            
+            DispatchQueue.main.async {
+                decodedResponse.results.forEach { photoResult in
+                    let newPhoto = Photo(
+                        id: photoResult.id,
+                        size: CGSize(width: CGFloat(photoResult.width), height: CGFloat(photoResult.height)),
+                        createdAt: photoResult.createdAt,
+                        welcomeDescription: photoResult.description,
+                        thumbImageURL: photoResult.urls.thumb,
+                        largeImageURL: photoResult.urls.full,
+                        isLiked: false
+                    )
+                    self.photos.append(newPhoto)
+                }
+                self.lastLoadedPage = nextPage
+                NotificationCenter.default.post(name: ImagesListService.imageListUpdated,
+                                                object: self,
+                                                userInfo: ["ImageLoaded": "Page \(nextPage)"])
+            }
+                
+            task?.resume()
+            
+        }
     }
-}
+        
+        func makeFetchListPhotoRequest(username: String, page: Int) -> URLRequest? {
+            URLRequest.makeHTTPRequest(
+                path: "/users/\(username)\(page)",
+                httpMethod: "GET",
+                baseURL: PhotoListUrl)
+        }
+    }
+
--- Structures/PhotoStructures.swift
+++ Structures/PhotoStructures.swift
@@ -8,25 +8,29 @@
 import Foundation
 
 struct Photo {
-    let id: String
-    let size: CGSize
-    let createdAt: String?
-    let welcomeDescription: String?
-    let thumbImageURL: URL
-    let largeImageURL: URL
-    let isLiked: Bool
+    var id: String
+    var size: CGSize
+    var createdAt: String?
+    var welcomeDescription: String?
+    var thumbImageURL: String
+    var largeImageURL: String
+    var isLiked: Bool
 }
 
-struct PhotoResult: Decodable {
+struct PhotoPageResult: Decodable {
     let id: String
     let width, height: Int
     let createdAt: String?
     let description, altDescription: String?
     let urls: UrlsResult
-    let likes: Int
-    let likedByUser: Bool
+//    let likes: Int
+//    let likedByUser: Bool
 }
 
+struct PhotoPageResponse: Decodable {
+    let results: [PhotoPageResult]
+}
 struct UrlsResult: Decodable {
-    let raw, full, regular, small, thumb: String
+    let full: String
+    let thumb: String
 }
