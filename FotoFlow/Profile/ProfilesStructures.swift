5e91c2f6d46471b745b1c1e8093e1f3f271828d7
ValeriiaPetrosian <petrosyan.valeria@gmail.com>
2024-05-13 20:40:54 +0100

    Constants+Profile

--- FotoFlow/Profile/ProfilesStructures.swift
+++ FotoFlow/Profile/ProfilesStructures.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 struct ProfileResult: Codable {
-    let userLogin: String
-    let firstName: String
-    let lastName: String
-    let bio: String?
-    let profileImage: ProfileImage?
+    var userLogin: String
+    var firstName: String
+    var lastName: String
+    var bio: String?
+    var profileImage: ProfileImage?
     
     private enum CodingKeys: String, CodingKey {
         case userLogin = "username"
@@ -24,10 +24,10 @@ struct ProfileResult: Codable {
 }
     
     struct Profile {
-        let username: String
-        let name: String
-        let loginName: String
-        let bio: String?
+        var username: String
+        var name: String
+        var loginName: String
+        var bio: String?
         
     }
         struct ProfileImage: Codable {
